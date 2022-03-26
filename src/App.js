ebb515639a0f7c4277b092a7e91c72c02fc5dcfd
Mohamed Adel Zidan <mohamed.adel.zidane@gmail.com>
2022-03-26 21:34:33 +0200

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import NavBar from './Components/Navbar/Navbar';
 function App() {
   return (
     <div className="App backgroundImg">
-     <h1>Weather Apps</h1>
+     <NavBar/>
     </div>
   );
 }
